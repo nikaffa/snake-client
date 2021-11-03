64685591716e41d75a97ddf401c7dde580c4a8b1
Veronika Fatikhova <fidelio@list.ru>
2021-11-03 14:43:48 -0700

    client.js added, name added

--- play.js
+++ play.js
@@ -1,21 +1,2 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({ //creates an object representing connection with the server
-    host: '165.227.47.243',// IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //prints out the greeting from server once being connected
-  conn.on('connect', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const { connect } = require("./client");
