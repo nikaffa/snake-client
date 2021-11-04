35f59f224cb3b35421d40e81f66d5215a7c0bbf1
Veronika Fatikhova <fidelio@list.ru>
2021-11-03 18:43:09 -0700

    play modified, event handlers added

--- play.js
+++ play.js
@@ -1,2 +1,20 @@
-const net = require("net");
+//Establishes UI (user interface) where players can send keyboard commands
+//const net = require("net");
 const { connect } = require("./client");
+
+// setup UI to handle user inputs from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); //on any input from stdin, call the callback
+  return stdin;
+};
+//when user inputs 'ctrl+c', exit the program
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+setupInput();
